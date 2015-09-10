ea6ca4c1206c834aba47f3d7fccf6ce03e7eda60
dbellamy <damien.bellamy@polyconseil.fr>
2015-09-10 14:25:15 +0200

    Added time check

--- BASH/Scripts/Monitoring/check_invoice_primobox.sh
+++ BASH/Scripts/Monitoring/check_invoice_primobox.sh
@@ -8,15 +8,25 @@ STATE_UNKNOWN=3
 MANAGE_PY='django-admin.py'
 
 # Prepare the command
+export http_proxy=http://proxy.infra.autolib.eu:8080/
+export ftp_proxy=http://proxy.infra.autolib.eu:8080/
+export https_proxy=http://proxy.infra.autolib.eu:8080/
 export DJANGO_SETTINGS_MODULE=autoslave.settings.single
 export PYTHONPATH=/usr/share/autoslave
 CMD="${MANAGE_PY}"
 
-TEST=$($CMD check_invoice_pdf)
+TEST=$(/usr/bin/django-admin.py check_invoice_pdf)
+TIME=$(/usr/bin/django-admin.py check_invoice_pdf | grep -Eo '.\...')
+
+TIMEINT=$(echo $TIME | sed s/"\."//g)
+
+if [[ $TIMEINT -ge 400 ]]; then
+    echo "CRITICAL: PDF download took $TIME seconds | time_in_sec="$TIME""; exit ${STATE_CRITICAL}
+fi
 
 case "$TEST" in
-        OK)  echo "OK: PDF download success | state=1"; exit ${STATE_OK}
+        OK*)  echo "OK: PDF download success | time_in_sec="$TIME""; exit ${STATE_OK}
         ;;
-        *)  echo "CRITICAL: PDF download timeout | state=0"; exit ${STATE_OK}
+        *)  echo "CRITICAL: PDF download timeout | time_in_sec="$TIME""; exit ${STATE_OK}
         ;;
 esac
